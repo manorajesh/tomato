411d655e3dea018bb309cde574d868735859014e
manorajesh <mano@raje.sh>
2024-10-29 21:02:56 -0400

    Initial Commit

---
+++ tomato Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tomato Watch App
+//
+//  Created by Mano Rajesh on 10/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tomato Watch App/tomatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomatoApp.swift
+//  tomato Watch App
+//
+//  Created by Mano Rajesh on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct tomato_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
